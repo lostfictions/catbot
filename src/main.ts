6988294b044c1d93b18781c7b597ff76fbb249c9
sa <lostfictions@users.noreply.github.com>
2017-12-24 02:33:58 -0500

    reorganize masto functions into a more library-like structure

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,55 @@
 require('source-map-support').install() // tslint:disable-line:no-require-imports
 
 import { makeHeathcliff } from './heath'
-import { toot } from './toot'
+import { toot } from './twoot/toot'
+// import { tweet } from './twoot/tweet'
+
+import {
+  MASTODON_SERVER,
+  MASTODON_TOKEN,
+  // TWITTER_CONSUMER_KEY as consumerKey,
+  // TWITTER_CONSUMER_SECRET as consumerSecret,
+  // TWITTER_ACCESS_KEY as accessKey,
+  // TWITTER_ACCESS_SECRET as accessSecret,
+  isValidMastodonConfiguration,
+  // isValidTwitterConfiguration
+} from './env'
 
 setInterval(
-  () => (async () => {
+  (async () => {
     const cliff = await makeHeathcliff()
-    const res = await toot({ status: "Today's Heathcliff:", attachments: [cliff] })
-    console.log(`Tooted a Heath: ${res.url}`)
-  })().catch(e => {
-    console.error(e)
+    const status = "Today's Heathcliff:"
+    if(isValidMastodonConfiguration) {
+      try {
+        const postedToot = await toot({
+          status,
+          attachments: [cliff],
+          token: MASTODON_TOKEN,
+          server: MASTODON_SERVER
+        })
+        console.log(`Tooted a Heath: ${postedToot.url}`)
+      }
+      catch(e) {
+        console.error('error while trying to toot: ', e)
+      }
+    }
+    // if(isValidTwitterConfiguration) {
+    //   try {
+    //     const t = await tweet({
+    //       consumerKey,
+    //       consumerSecret,
+    //       accessKey,
+    //       accessSecret,
+    //       status,
+    //       attachments: [cliff]
+    //     })
+
+    //     console.log(`Tweeted! ${t.created_at}`)
+    //   }
+    //   catch(e) {
+    //     console.error('error while trying to tweet: ', e)
+    //   }
+    // }
   }),
   1000 * 60 * 60 * 6 // Every six hours
 )
--- src/twoot/toot.ts
+++ src/twoot/toot.ts
@@ -4,50 +4,51 @@ import { stringify as querystringify } from 'querystring'
 import * as FormData from 'form-data'
 import * as got from 'got'
 
-import { Attachment, Status } from './mastodon'
-
-import {
-  MASTODON_SERVER,
-  MASTODON_TOKEN
-} from './env'
+import { Attachment, Status } from './mastodon-api/mastodon'
 
 const API_POSTFIX = '/api/v1/'
-const MASTODON_API_URL = MASTODON_SERVER + API_POSTFIX
 const ENDPOINTS = {
   postStatus: 'statuses',
   uploadMedia: 'media'
 }
 
-const mastoAPI: { [k in keyof typeof ENDPOINTS]: string } = {} as any
-for(const [k, v] of Object.entries(ENDPOINTS)) {
-  (mastoAPI as any)[k] = MASTODON_API_URL + v
+function getMastodonEndpoint(server: string, endpoint: keyof typeof ENDPOINTS): string {
+  return server + API_POSTFIX + ENDPOINTS[endpoint]
 }
 
 export interface TootOptions {
+  token: string
   status: string
   attachments?: string[]
+  server?: string
 }
 
-export async function toot({ status, attachments = [] }: TootOptions): Promise<Status> {
+export async function toot({
+  token,
+  status,
+  attachments = [],
+  server = 'https://mastodon.social'
+}: TootOptions): Promise<Status> {
+
   const mediaIds = await Promise.all(attachments.map(async path => {
     const formData = new FormData()
     formData.append('file', fs.createReadStream(path))
-    const mediaRes = await got(mastoAPI.uploadMedia, {
+
+    const mediaRes = await got(getMastodonEndpoint(server, 'uploadMedia'), {
       body: formData,
-      headers: { Authorization: `Bearer ${MASTODON_TOKEN}` }
+      headers: { Authorization: `Bearer ${token}` }
     })
 
     return (JSON.parse(mediaRes.body) as Attachment).id
   }))
 
-
   // We have to encode the querystring ourselves because of a quirk with the
   // Mastodon API. From the docs:
   //
-  // When an array parameter is mentioned, the Rails convention of specifying
+  // "When an array parameter is mentioned, the Rails convention of specifying
   // array parameters in query strings is meant. For example, a ruby array like
   // foo = [1, 2, 3] should be encoded in the params as foo[]=1&foo[]=2&foo[]=3,
-  // with empty square brackets.
+  // with empty square brackets."
   //
   // (https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#parameter-types)
   const query: any = { status }
@@ -55,10 +56,10 @@ export async function toot({ status, attachments = [] }: TootOptions): Promise<S
     query['media_ids[]'] = mediaIds
   }
 
-  const res = await got(mastoAPI.postStatus, {
+  const res = await got(getMastodonEndpoint(server, 'postStatus'), {
     body: querystringify(query),
     headers: {
-      Authorization: `Bearer ${MASTODON_TOKEN}`
+      Authorization: `Bearer ${token}`
     }
   })
 
