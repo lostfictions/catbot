2e9739292149ec27895cebce7438cc37e85200e9
sa <lostfictions@users.noreply.github.com>
2017-12-24 02:24:29 -0500

    add better validation of api tokens

--- src/env.ts
+++ src/env.ts
@@ -7,7 +7,10 @@ const env = envalid.cleanEnv(
     DATA_DIR: envalid.str({ default: 'persist' }),
     MASTODON_SERVER: envalid.url({ default: 'https://mastodon.social' }),
     MASTODON_TOKEN: envalid.str({ default: '' }),
-    TWITTER_TOKEN: envalid.str({ default: '' })
+    TWITTER_CONSUMER_KEY: envalid.str({ default: '' }),
+    TWITTER_CONSUMER_SECRET: envalid.str({ default: '' }),
+    TWITTER_ACCESS_KEY: envalid.str({ default: '' }),
+    TWITTER_ACCESS_SECRET: envalid.str({ default: '' })
   },
   { strict: true }
 )
@@ -16,19 +19,36 @@ export const {
   DATA_DIR,
   MASTODON_SERVER,
   MASTODON_TOKEN,
-  TWITTER_TOKEN
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  TWITTER_ACCESS_KEY,
+  TWITTER_ACCESS_SECRET
 } = env
 
 if(!fs.existsSync(DATA_DIR)) {
   throw new Error(`Data directory '${DATA_DIR}' doesn't exist!`)
 }
 
-const isValidConfiguration = MASTODON_TOKEN || TWITTER_TOKEN
+export const isValidMastodonConfiguration = MASTODON_TOKEN.length > 0
+export const isValidTwitterConfiguration = (
+  TWITTER_CONSUMER_KEY &&
+  TWITTER_CONSUMER_SECRET &&
+  TWITTER_ACCESS_KEY &&
+  TWITTER_ACCESS_SECRET
+)
 
-if(!isValidConfiguration) {
-  console.error(`Invalid environment config! Bot will do nothing if no Mastodon or Twitter API token is present.`)
+if(!isValidMastodonConfiguration && !isValidTwitterConfiguration) {
+  console.error(`Invalid environment config!`)
+  console.error(`Bot will do nothing if no Mastodon API token (or Twitter API token set) is present.`)
+  console.error(`Note that all four Twitter token types must be present for Twitter to work.`)
 
-  const varsToCheck: (keyof typeof env)[] = ['MASTODON_TOKEN', 'TWITTER_TOKEN']
+  const varsToCheck: (keyof typeof env)[] = [
+    'MASTODON_TOKEN',
+    'TWITTER_CONSUMER_KEY',
+    'TWITTER_CONSUMER_SECRET',
+    'TWITTER_ACCESS_KEY',
+    'TWITTER_ACCESS_SECRET'
+  ]
   const configInfo = varsToCheck.map(key => `${key}: ${env[key] ? 'OK' : 'NONE'}`).join('\n')
   throw new Error(configInfo)
 }
