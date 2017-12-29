e014c769836e0d0c922e02fa400c7a2454d239b9
sa <lostfictions@users.noreply.github.com>
2017-12-29 01:10:25 -0500

    names, venery terms

--- src/text.ts
+++ src/text.ts
@@ -3,21 +3,23 @@ import * as path from 'path'
 
 import { randomInArray } from './util'
 
-const preferredAdjs = fs.readFileSync(path.join(__dirname, '../text/adjs_preferred.txt'))
+const processFile = (filePath: string): string[] => fs.readFileSync(path.join(__dirname, filePath))
   .toString()
   .split('\n')
   .map(s => s.trim())
   .filter(line => line.length > 0)
 
-const adjs = fs.readFileSync(path.join(__dirname, '../text/adjs.txt'))
-  .toString()
-  .split('\n')
-  .map(s => s.trim())
-  .filter(line => line.length > 0)
+
+const preferredAdjs = processFile('../text/adjs-preferred.txt')
+const adjs = processFile('../text/adjs.txt')
+const nouns = processFile('../text/nouns.txt')
+const names = processFile('../text/names.txt')
+const veneryTerms = processFile('../text/venery-terms.txt')
 
 const joiners = [', ', ' but ', ' and ']
 
 const getAdj = () => randomInArray(Math.random() < 0.25 ? adjs : preferredAdjs)
+const getName = () => randomInArray(Math.random() < 0.25 ? nouns : names)
 
 const an = (word: string) => {
   switch(true) {
@@ -50,9 +52,13 @@ export function makeStatus(catsMade: number): string {
       case 6: return 'six'
       case 7: return 'seven'
       case 8: return 'eight'
-      default: return 'a kaboodle of'
+      default: return `a ${randomInArray(veneryTerms)} of`
     }
   })(catsMade)
 
-  return `${prefix} ${adj} ${catsMade === 1 ? 'cat' : 'cats'}:`
+  const maybeNames = catsMade >= 2 && catsMade <= 4 && Math.random() < 0.18
+    ? ` (${[...Array(catsMade)].map(getName).join(', ')})`
+    : ''
+
+  return `${prefix} ${adj} ${catsMade === 1 ? 'cat' : 'cats'}${maybeNames}:`
 }
