b51e2582820165dfcd7498f821f813971d16600d
lostfictions <lostfictions@users.noreply.github.com>
2018-07-14 19:23:19 -0400

    better faculties for running locally

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,42 @@
-require('source-map-support').install() // tslint:disable-line:no-require-imports
+require("source-map-support").install();
 
-import { makeCat } from './catmaker'
-import { twoot, Configs as TwootConfigs } from 'twoot'
-import { makeStatus } from './text'
+import { makeCat } from "./catmaker";
+import { twoot, Configs as TwootConfigs } from "twoot";
+import { makeStatus } from "./text";
 
-import { randomInt, randomByWeight } from './util'
+import { randomInt, randomByWeight } from "./util";
 
 import {
   MASTODON_SERVER,
   MASTODON_TOKEN,
   isValidMastodonConfiguration,
-
   TWITTER_CONSUMER_KEY as consumerKey,
   TWITTER_CONSUMER_SECRET as consumerSecret,
   TWITTER_ACCESS_KEY as accessKey,
   TWITTER_ACCESS_SECRET as accessSecret,
   isValidTwitterConfiguration,
-
   INTERVAL_MINUTES
-} from './env'
+} from "./env";
 
-const twootConfigs: TwootConfigs = []
-if(isValidMastodonConfiguration) {
+const twootConfigs: TwootConfigs = [];
+if (isValidMastodonConfiguration) {
   twootConfigs.push({
     token: MASTODON_TOKEN,
     server: MASTODON_SERVER
-  })
+  });
 }
-if(isValidTwitterConfiguration) {
+if (isValidTwitterConfiguration) {
   twootConfigs.push({
     consumerKey,
     consumerSecret,
     accessKey,
     accessSecret
-  })
+  });
 }
 
-async function doTwoot(): Promise<void> {
+async function makeTwoot(): Promise<{ status: string; filename: string }> {
   const { filename, catsMade } = await makeCat({
-    catChance: randomByWeight([
-      [80, 10],
-      [90, 5],
-      [100, 10]
-    ]),
+    catChance: randomByWeight([[80, 10], [90, 5], [100, 10]]),
     leftChance: randomByWeight([
       [0, 1],
       [randomInt(50), 0],
@@ -61,31 +55,40 @@ async function doTwoot(): Promise<void> {
       [50, 20],
       [randomInt(50, 100), 10]
     ]),
-    minSteps: randomByWeight([
-      [1, 1],
-      [randomInt(5, 15), 20],
-      [50, 1]
-    ]),
-    maxSteps: randomByWeight([
-      [1, 1],
-      [randomInt(30, 60), 20],
-      [100, 1]
-    ])
-  })
+    minSteps: randomByWeight([[1, 1], [randomInt(5, 15), 20], [50, 1]]),
+    maxSteps: randomByWeight([[1, 1], [randomInt(30, 60), 20], [100, 1]])
+  });
 
-  const status = makeStatus(catsMade)
+  const status = makeStatus(catsMade);
+
+  return { status, filename };
+}
+
+async function doTwoot(): Promise<void> {
+  const { status, filename } = await makeTwoot();
 
   try {
-    const urls = await twoot(twootConfigs, status, [filename])
-    console.log(`[${new Date().toISOString()}] twooted:\n${urls.map(u => '\t -> ' + u).join('\n')}`)
-  }
-  catch(e) {
-    console.error('error while trying to twoot: ', e)
+    const urls = await twoot(twootConfigs, status, [filename]);
+    console.log(
+      `[${new Date().toISOString()}] twooted:\n${urls
+        .map(u => "\t -> " + u)
+        .join("\n")}`
+    );
+  } catch (e) {
+    console.error("error while trying to twoot: ", e);
   }
 }
 
-doTwoot().then(() => { console.log('Made initial twoots.') })
+if (process.argv.slice(2).includes("local")) {
+  makeTwoot().then(({ status, filename }) =>
+    console.log(status, `file://${filename}`)
+  );
+} else {
+  doTwoot().then(() => {
+    console.log("Made initial twoots.");
+  });
 
-setInterval(doTwoot, 1000 * 60 * INTERVAL_MINUTES)
+  setInterval(doTwoot, 1000 * 60 * INTERVAL_MINUTES);
 
-console.log('Catbot is running.')
+  console.log("Catbot is running.");
+}
